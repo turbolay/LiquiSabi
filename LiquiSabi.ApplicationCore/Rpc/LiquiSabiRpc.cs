91c3d9439e215bf7261513b4bf18d167db111f62
Turbolay <turbolay@proton.me>
2024-10-05 09:20:59 -0600

    Average instead of sumary

--- LiquiSabi.ApplicationCore/Rpc/LiquiSabiRpc.cs
+++ LiquiSabi.ApplicationCore/Rpc/LiquiSabiRpc.cs
@@ -11,32 +11,32 @@ public class LiquiSabiRpc : IJsonRpcService
         return CoinjoinStore.GetSavedRounds(since, until, coordinatorEndpoint);
     }
     
-    [JsonRpcMethod("summary")]
+    [JsonRpcMethod("average")]
     public CoinjoinStore.SavedRound GetSummary(DateTimeOffset? since = null, DateTimeOffset? until = null, string? coordinatorEndpoint = null)
     {
         var rounds = GetRounds(since, until, coordinatorEndpoint).ToList();
         return new CoinjoinStore.SavedRound(
             CoordinatorEndpoint: string.Join(';', rounds.Select(x => x.CoordinatorEndpoint).Distinct()),
-            EstimatedCoordinatorEarningsSats: rounds.Sum(x => x.EstimatedCoordinatorEarningsSats),
+            EstimatedCoordinatorEarningsSats: (int)rounds.Average(x => x.EstimatedCoordinatorEarningsSats),
             RoundId: rounds.Count.ToString(),
             IsBlame: false,
             CoordinationFeeRate: Math.Round(rounds.Average(x => x.CoordinationFeeRate), 4),
-            MinInputCount: (int)Math.Round(rounds.Average(x => x.MinInputCount)),
+            MinInputCount: (int)rounds.Average(x => x.MinInputCount),
             ParametersMiningFeeRate: Math.Round(rounds.Average(x => x.ParametersMiningFeeRate), 2),
             RoundStartTime: rounds.Min(x => x.RoundStartTime),
             RoundEndTime: rounds.Max(x => x.RoundEndTime),
             TxId: rounds.Count.ToString(),
             FinalMiningFeeRate: Math.Round(rounds.Average(x => x.FinalMiningFeeRate), 2),
-            VirtualSize: (int)Math.Round(rounds.Average(x => x.VirtualSize)),
-            TotalMiningFee: rounds.Sum(x => x.TotalMiningFee),
-            InputCount: rounds.Sum(x => x.InputCount),
-            TotalInputAmount: rounds.Sum(x => x.TotalInputAmount),
-            FreshInputsEstimateBtc: rounds.Sum(x => x.FreshInputsEstimateBtc),
+            VirtualSize: (int)rounds.Average(x => x.VirtualSize),
+            TotalMiningFee: (long)rounds.Average(x => x.TotalMiningFee),
+            InputCount: (int)rounds.Average(x => x.InputCount),
+            TotalInputAmount: (int)rounds.Average(x => x.TotalInputAmount),
+            FreshInputsEstimateBtc: Math.Round(rounds.Average(x => x.FreshInputsEstimateBtc), 8),
             AverageStandardInputsAnonSet: Math.Round(rounds.Average(x => x.AverageStandardInputsAnonSet), 2),
-            OutputCount: rounds.Sum(x => x.OutputCount),
-            TotalOutputAmount: rounds.Sum(x => x.TotalOutputAmount),
+            OutputCount: (int)rounds.Average(x => x.OutputCount),
+            TotalOutputAmount: (long)rounds.Average(x => x.TotalOutputAmount),
             ChangeOutputsAmountRatio: Math.Round(rounds.Average(x => x.ChangeOutputsAmountRatio), 2),
             AverageStandardOutputsAnonSet: Math.Round(rounds.Average(x => x.AverageStandardOutputsAnonSet), 2),
-            TotalLeftovers: rounds.Sum(x => x.TotalLeftovers));
+            TotalLeftovers: (int)rounds.Average(x => x.TotalLeftovers));
     }
 }
